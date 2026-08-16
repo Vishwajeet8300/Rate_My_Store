178d47e7c71c30b00607d16817d909454a11942a
Vishwajeet <vishwajeet@gmail.com>
2026-08-16 10:27:13 +0530

    Prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const db = require("./db");
 const app = express();
 
 const PORT = process.env.PORT || 5000;
-const HOST = "127.0.0.1";
+const HOST = "0.0.0.0";
 
 // =====================================================
 // MIDDLEWARE
